fa91f609bb57278372bea698063f81a993ba80a8
davegregg <davegregg@gmail.com>
2017-08-28 09:00:23 -0700

    Initial code check

--- revised_oregon_trail.js
+++ revised_oregon_trail.js
@@ -1,8 +1,8 @@
-
+// NOTE: Use the Atom package 'Todo-Show' to see the notes I've added all together in one place. If you have the Todo-Show package, go to Packages > Todo-Show > Find in Active File
 (function(){
 
 function getRandomIntInclusive(min, max){
-  main = Math.ceil(min)
+  main = Math.ceil(min) // FIXME: Typo `main` > `min`
   max = Math.floor(max);
   return Math.floor(Math.random()* (max - min + 1)) + min;
 }
@@ -30,7 +30,7 @@ function Wagon() {
 //Create a new traveler object with the specified name, a random amount of food, and isHealthy = true.
 makeTraveler= function(travelerName){
 
-Traveler.name = travelerName;
+Traveler.name = travelerName; // FIXME: You want to be initializing an instance of the Traveler class. Traveler is our abstract concept, our pattern which all the new Travelers will follow. So when you do something like Traveler.name = 'foo', what you are doing is setting a property on the "abstract" Traveler object itself -- not on any particular traveler. So assign `new Traveler()` to a variable and set a `name` property to it. This applies for `food`, `isHealthy`.
 let randomFood = getRandomIntInclusive(5,100);
 Traveler.food = randomFood;
 Traveler.isHealthy = true;
@@ -43,22 +43,22 @@ console.log("make healthy" + Traveler.isHealthy);
 
 //Create a new wagon with an empty passenger list and the specified capacity.
   makeWagon= function(capacity){
-  Wagon.capacity = capacity;
+  Wagon.capacity = capacity;  // FIXME: This will also need you to use `new` to initialize an instance of the Wagon class. And then you'll set the capacity of the particular wagon you've assigned to a variable.
   Wagon.passengerList = [];
   console.log("made wagon with capacity" + Wagon.capacity )
     }
 
 //    Create a new wagon with an empty passenger list and the specified capacity.
 
-hunt= function(){
+hunt= function(){ // FIXME: This function will need you to pass in an argument, which will represent your traveler, so that the function knows WHO is hunting. The NewLine path says it expects a syntax like `hunt(traveler)`. You can do it without passing in the particular traveler, but you'd have to make the hunt function a method assigned to a hunt property on the Traveler object, so that all travelers can do something like `traveler4.hunt()`. Either way would be fine. The latter would be closer to best practice. The former would be matching the NewLine requirement (which is totally fine for right now). Pick whichever you're comfortable with at the moment.
 
-console.log("initial food:" + Traveler.food);
+console.log("initial food:" + Traveler.food);  // FIXME: Remember: you'll want to be checking the `food` property of the particular traveler, not of the Traveler class object itself. As I said above, you'll either need to pass in the particular traveler as an argument, or you'll need to make the `hunt()` a method attached to the Traveler class object -- whichever you're comfortable doing right now.
 let foodIncrease = Math.random() < 0.5 ? true : false;
 console.log("increase by 100 percent is" + this.foodIncrease);
 
 if (this.foodIncrease) {
 
-  Traver.food = Traver.food * 2;
+  Traver.food = Traver.food * 2; // NOTE: This would throw an error "Traver is not defined". Just a simple misspelling. But as above, you won't actually want to be checking the Traveler class object's food property.
 }
   else {
 
@@ -73,7 +73,7 @@ if (this.foodIncrease) {
 
   //  eat(traveler)
 //Consumes 20 of the traveler's food. If the traveler doesn't have 20 food, the traveler is no longer healthy.
-    eat= function(){
+    eat= function(){ // FIXME: Same issue as with `hunt()`
 
     let foodStart = Traveler.food;
     newfood = foodStart - (foodStart * .20);
@@ -86,10 +86,10 @@ if (this.foodIncrease) {
   }
 
   //Add a travelerto a wagon
-  join = function(wagon, traveler){
+  join = function(wagon, traveler){ // FIXME: This is what you want `eat()` and `hunt()` to look like, only without the `wagon` argument.
 
     if (Wagon.capacity != 0){
-
+      // FIXME: Nothing is happening here yet, which you probably know.
     }
     Wagon.passengerList.push(traveler);
 
